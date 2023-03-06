e5aee6952ac43259d32bb9539b7b85df835d1d59
Roman Yakobnyuk <roman.yakobnyuk@gmail.com>
2023-03-06 16:44:03 +1000

    Initial commit - Server working

---
+++ README.md
@@ -0,0 +1,7 @@
+## Introduction
+
+Simple chat application backend using Node.js, Express and Redis.
+
+## Prepare for production
+
+docker run -p 3000:3000 --name chat-app
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Redis = require('ioredis');
+
+const redis = new Redis();
+
+const app = express();
+
+app.use(bodyParser.json());
+
+function isValidString(string) {
+    return string && typeof string === 'string' && string.length > 0 && string.length < 256;
+}
+
+app.post('/chat', async (req, res) => {
+  const { author, message } = req.body;
+
+  if (isValidString(author) === false || isValidString(message) === false) {
+    res.status(400).send('Invalid request body');
+    return;
+  }
+
+  const timestamp = Date.now();
+
+  try {
+    await redis.lpush('chat-messages', JSON.stringify({ author, message, timestamp }));
+    await redis.ltrim('chat-messages', 0, 19); // keep only the latest 20 messages
+    res.status(200).send('Message saved');
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Failed to save the message');
+  }
+});
+
+app.get('/chat', async (req, res) => {
+  try {
+    const messages = await redis.lrange('chat-messages', 0, -1);
+    res.status(200).send(messages.map(JSON.parse));
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error getting messages from Redis');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
